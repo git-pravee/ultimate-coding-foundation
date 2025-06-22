780302093992e8014511dd438c5ea7697e1449e7
git-pravee <praveent2060@gmail.com>
2025-06-22 23:22:27 +0530

    Decision control statements

---
+++ C programming/assignment/assign7.c
@@ -0,0 +1,59 @@
+/*
+1. Write a program to check whether a given number is positive or non positive. 
+2. Write a program to check whether a given number is divisible by 5 or not. 
+3. Write a program to check whether a given number is an even number or an odd number. 
+4. Write a program to check whether a given number is an even number or an odd number without using % operator. 
+5. Write a program to check whether the given number is even or odd using a bitwise operator.
+*/
+
+#include<stdio.h>
+void p1()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(n>0)
+        printf("Positive number");
+    else
+        printf("Non Positive number");
+}
+void p2()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(n%5)
+        printf("Not divisible 5");
+    else
+        printf("Divisible 5");
+}
+void p3()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(n%2)
+        printf("Odd number");
+    else
+        printf("Even number");
+}
+void p4()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(n/2*2 == n)
+        printf("Even number");
+    else
+        printf("Odd number");
+}
+void p5()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(n&1)
+        printf("Odd number");
+    else
+        printf("Even number");
+}
