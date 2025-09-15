ee8671802f463858a2bcdd84b3b4a51153533c07
git-pravee <praveent2060@gmail.com>
2025-09-16 00:00:58 +0530

    assign20 quetions uploaded

---
+++ C programming/assignment/assign20.c
@@ -0,0 +1,25 @@
+/*
+1. Write a program to find and display grade obtained by a student in a test. 
+   Grading specifications are as follows:
+   - Marks from 90 to 100 : Grade A
+   - Marks from 80 to less than 90 : Grade B
+   - Marks from 70 to less than 80 : Grade C
+   - Marks from 60 to less than 70 : Grade D
+   - Marks from 50 to less than 60 : Grade E
+   - Marks from below 50 : Grade F
+   - Marks greater than 100 or less than 0 : Inavalid marks
+2. Write a menu driven program with the following options:
+   1. Factorial of a number
+   2. Check Even and Odd
+   3. Area of circle
+   4. Sum of first N natural numbers
+   5. Exit
+3. Write a program to check whether a given character is uppercase alphabet or lower case alphabet or some other special character, using switch case statemetnt.
+4. Write a program to check whether a given character is vowel of consonant or some other special character, using switch case statement.
+5. Write a menu driven program with the following options:
+   1. Calculate LCM of two numbers
+   2. Calculate sum of the digits of a number
+   3. Volumbe of cuboid
+   4. Check whether a given number is Prime or not
+   5. Exit
+*/
