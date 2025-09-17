642c69dfd4b706281183d4e2c0b9123039d043a1
git-pravee <praveent2060@gmail.com>
2025-09-17 22:17:39 +0530

    two-quetions-solved

--- C programming/assignment/assign20.c
+++ C programming/assignment/assign20.c
@@ -15,7 +15,7 @@
    4. Sum of first N natural numbers
    5. Exit
 3. Write a program to check whether a given character is uppercase alphabet or lower case alphabet or some other special character, using switch case statemetnt.
-4. Write a program to check whether a given character is vowel of consonant or some other special character, using switch case statement.
+4. Write a program to check whether a given character is vowel or consonant or some other special character, using switch case statement.
 5. Write a menu driven program with the following options:
    1. Calculate LCM of two numbers
    2. Calculate sum of the digits of a number
@@ -122,8 +122,89 @@ void p2()
         }
     }
 }
+void p3()
+{
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    switch(ch)
+    {
+        case 'A' ... 'Z':
+            printf("Uppercase letter\n");
+            break;
+        case 'a' ... 'z':
+            printf("Lowercase letter\n");
+            break;
+        case '0' ... '9':
+            printf("Number\n");
+            break;
+        default:
+            printf("Special character\n");
+    }
+}
+void p4()
+{
+    char ch;
+    printf("Enter a alphabet: ");
+    scanf("%c", &ch);
+    switch(ch)
+    {
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+            printf("It is a vowel\n");
+            break;
+        case 'B' ... 'D': case 'F' ... 'H': case 'J' ... 'N': case 'P' ... 'T': case 'V' ... 'Z':
+        case 'b' ... 'd': case 'f' ... 'h': case 'j' ... 'n': case 'p' ... 't': case 'v' ... 'z':
+            printf("Its is a consonent\n");
+            break;
+        default:
+            printf("Special character\n");
+    }
+}
+oid p2()
+{
+    int status=1;
+    while(status!=5)
+    {
+        printf("\nEnter a choice: \n");
+        printf("1. Calculate LCM of two numbers\n");
+        printf("2. Calculate sum of the digits of a number\n");
+        printf("3. Volumbe of cuboid\n");
+        printf("4. Check whether a given number is Prime or not\n");
+        printf("5. Exit\n");
+        scanf("%d", &status);
+        switch(status)
+        {
+            case 1:
+                {
+                    
+                }
+                break;
+            case 2:
+                {
+                    
+                }
+                break;
+            case 3:
+                {
+                    
+                }
+                break;
+            case 4:
+                {
+                    
+                }
+                break;
+            case 5:
+                printf("Exiting program...\n");
+                break;
+            default:
+                printf("Invalid option! Please try again..\n");
+        }
+    }
+}
 int main()
 {
-    p2();
+    p4();
     return 0;
 }
