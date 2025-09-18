c45cd52953ff2fd1e3d228bc3a4f9f7627ba6569
git-pravee <praveent2060@gmail.com>
2025-09-18 23:15:55 +0530

    questions-assignment22

---
+++ C programming/assignment/assign22.c
@@ -0,0 +1,7 @@
+/*
+1. Write a function to calculate the factorial of a number. (TSRS)
+2. Write a function to calculate the number of combinations one can make from n items and r selected at a time. (TSRS)
+3. Write a function to calculate the number of arrangements one can make from n items and r selected at a time. (TSRS)
+4. Write a function to check whether a given number contains a given digit or not. (TSRS)
+5. Write a function to print all prime factors of a given number. For example, if the number is 36 then your result should be 2, 2, 3, 3. (TSRN)
+*/
