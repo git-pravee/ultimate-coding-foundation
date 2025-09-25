599ee377e972bc7c00c48fd48933a4593c7f3868
git-pravee <praveent2060@gmail.com>
2025-09-25 23:24:02 +0530

    quetions-assignment23

---
+++ C programming/assignment/assign23.c
@@ -0,0 +1,7 @@
+/*
+1. Write a function to calculate LCM of two numbers. (TSRS)
+2. Write a function to calculate HCF of two numbers. (TSRS)
+3. Write a function to check whether a given number is Prime or not. (TSRS)
+4. Write a function to find the next prime number of a given number. (TSRS)
+5. Write a function to print first N prime numbers. (TSRN) 
+*/
