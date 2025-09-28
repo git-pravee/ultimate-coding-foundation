76bf6326451126a7de8ee2135deec05cbcc4c80d
git-pravee <praveent2060@gmail.com>
2025-09-28 21:43:10 +0530

    quetions-uploaded

---
+++ C programming/assignment/assign24.c
@@ -0,0 +1,12 @@
+/*
+1. Write a function to print all prime numbers between two given numbers. (TSRN)
+2. Write a function to print first N terms of Fibonacci series. (TSRN)
+3. Write a function to print PASCAL Triangle. (TSRN)
+4. Write a function to print all Armstrong numbers between two given numbers. (TSRN)
+5. Write a function to find the sum of first N terms of the series 1!/1+2!/2+3!/3+4!/4+5!/5...n!/n (TSRS)
+*/
+#include<stdio.h>
+int main()
+{
+    return 0;
+}
