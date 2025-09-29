c906ca203b38fdfff885077787be0d764d1ed99f
git-pravee <praveent2060@gmail.com>
2025-09-29 22:22:54 +0530

    first-quetions-solved

--- C programming/assignment/assign24.c
+++ C programming/assignment/assign24.c
@@ -6,7 +6,42 @@
 5. Write a function to find the sum of first N terms of the series 1!/1+2!/2+3!/3+4!/4+5!/5...n!/n (TSRS)
 */
 #include<stdio.h>
+int isPrime(int n)
+{
+    if(n<=1)
+        return 0;
+    int i=2;
+    for(i;i*i<=n;i++)
+        if(n%i==0)
+            return 0;
+    return 1;
+}
+int nextPrime(int n)
+{
+    while(n)
+        if(isPrime(++n))
+            return n;
+}
+void primeBetween(int a, int b)
+{
+    while(a<=b)
+    {
+        if(isPrime(a))
+        {
+            printf("%d ", a);
+            a++;
+        }
+        else
+            a=nextPrime(a);
+    }
+}
+void printFib(int n)
+{
+    
+}
 int main()
 {
+    primeBetween(0, 10);
+    printf("\n");
     return 0;
 }
