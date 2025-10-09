754d6c05d2f77b2efeb272325cbe98e6981b6fab
git-pravee <praveent2060@gmail.com>
2025-10-09 23:28:22 +0530

    quetions-assignment25

---
+++ C programming/assignment/assign25.c
@@ -0,0 +1,6 @@
+/*
+1. Write a recursive function to print first N natural numbers
+2. Write a recursive function to print first N natural numbers in reverse order
+3. Write a recursive function to print first N odd natural numbers
+4. Write a recursive function to print first N odd natural numbers in reverse order
+*/
