e9f9a3eb60ecca3b8b5cd44ebcb1c154f395a7e0
git-pravee <praveent2060@gmail.com>
2025-11-05 21:57:45 +0530

    recursive-function

--- C programming/assignment/assign25.c
+++ C programming/assignment/assign25.c
@@ -4,3 +4,22 @@
 3. Write a recursive function to print first N odd natural numbers
 4. Write a recursive function to print first N odd natural numbers in reverse order
 */
+#include<stdio.h>
+
+void f1(int n)
+{
+    if(n==1)
+    {
+        printf("%d ", n);
+        return; 
+    }
+    f1(n-1);
+    printf("%d ", n);
+
+}
+int main()
+{
+    f1(5);
+    printf("\n");
+    return 0;
+}
