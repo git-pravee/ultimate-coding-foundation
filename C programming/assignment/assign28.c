b4b4e28157de0b36a7fcd45c877f6c8390dd0150
git-pravee <praveent2060@gmail.com>
2025-11-09 22:58:54 +0530

    quetion

---
+++ C programming/assignment/assign28.c
@@ -0,0 +1,7 @@
+/*
+1. Write a recursive function to calculate factorial of given number
+2. Write a recursive function to calculate HCF of two numbers
+3. Write a recursive function to print first N terms of Fibonacci series
+4. Write a program in C to count the digits of given number using recursion
+5. Write a program in C to calculate the power of any number using recursion
+*/
