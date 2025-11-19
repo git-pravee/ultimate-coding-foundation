1e115147bc438647e2cbf06acd7bb87427657748
git-pravee <praveent2060@gmail.com>
2025-11-19 21:35:34 +0530

    complete-recursion

--- C programming/assignment/assign28.c
+++ C programming/assignment/assign28.c
@@ -5,3 +5,46 @@
 4. Write a program in C to count the digits of given number using recursion
 5. Write a program in C to calculate the power of any number using recursion
 */
+#include<stdio.h>
+int f1(int n)
+{
+    int fact = 1;
+    if(n==1)
+        return 1;
+    fact = n * f1(n-1);
+    return fact;
+}
+int f2(int a, int b)
+{
+    if(b==0)
+        return a;
+    return f2(b, a%b);
+}
+int fib(int n)
+{
+    if(n<=1)
+        return n;
+    return fib(n-1)+fib(n-2);
+}
+void f3(int n)
+{
+    for(int i=0; i<n; i++)
+        printf("%d ", fib(i));
+}
+int f4(int n)
+{
+    if(n%10==0) 
+        return 0;
+    return 1+f4(n/10); 
+}
+int f5(int a, int n)
+{
+    if(n==1)
+        return a;
+    return a * f5(a, n-1);
+}
+int main()
+{
+    printf("%d\n", f5(5,5));
+    return 0;
+}
