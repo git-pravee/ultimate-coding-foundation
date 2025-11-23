bef69c4e8da2e3c5fad66e6efdacc877a453cbb8
git-pravee <praveent2060@gmail.com>
2025-11-23 21:34:45 +0530

    quetions-assing33

---
+++ C programming/assignment/assign33.c
@@ -0,0 +1,14 @@
+/*
+1. Write a program to calculate the sum of two matrices each of order 3x3.
+2. Write a program to calculate the product of two matrices each of order 3x3.
+3. Write a program to print the transpose of given matrix.
+4. Four players are playing a tournament of chess with round robin method (each player will play with every other player).
+   Each win has 2 points, draw has 1 point and loose has 0 points. 
+   Declare a score_board two dimensional array to store the scores of the players agains each player.
+5. For que-4, define a function to update score_board after each match result.
+6. For que-4, define a function to display score_board.
+7. For que-4, define a function which returns the score of a specific player
+8. For que-4, define a function to find the winner of the tournament.
+9. For que-4, define a function to display rank of the players.
+10. For que-4, define a function to run tournament, in which user has to enter result of each game and update score board using score_board function.  
+*/
