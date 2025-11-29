bd7af74d80d047118b3c0655a5e41ad0366c2d60
git-pravee <praveent2060@gmail.com>
2025-11-29 17:39:16 +0530

    questions-solving

--- C programming/assignment/assign33.c
+++ C programming/assignment/assign33.c
@@ -26,10 +26,49 @@ void p1(int a[3][3], int b[3][3])
         printf("\n");
     }
 }
+void p2(int a[3][3], int b[3][3])
+{
+    int c[3][3];
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            int result=0;
+            for(int k=0; k<3; k++)
+                result=result+a[i][k]*b[k][j];
+            c[i][j]=result;
+        }
+    }
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+            printf("%3d ", c[i][j]);
+        printf("\n");
+    }
+}
+void p3(int a[3][3])
+{
+    int temp;
+    for(int i=0; i<3; i++)
+        for(int j=i+1; j<3; j++)
+        {
+            temp=a[i][j];
+            a[i][j]=a[j][i];
+            a[j][i]=temp;
+        }
+
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+            printf("%d ",a[i][j]);
+        printf("\n");
+    }
+}
+
 int main()
 {
     int a[3][3]={1,2,3,4,5,6,7,8,9};
     int b[3][3]={1,2,3,4,5,6,7,8,9};
-    p1(a,b);
+    
     return 0;
 }
